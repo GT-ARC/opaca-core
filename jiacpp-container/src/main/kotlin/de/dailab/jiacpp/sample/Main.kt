e305d04b81672408d76d2c0ccd631755a8b167cb
Tobias Küster <tobias.kuester@dai-labor.de>
2022-10-26 11:31:33 +0200

    helper method to read agent container image config from file

--- jiacpp-container/src/main/kotlin/de/dailab/jiacpp/sample/Main.kt
+++ jiacpp-container/src/main/kotlin/de/dailab/jiacpp/sample/Main.kt
@@ -2,21 +2,12 @@ package de.dailab.jiacpp.sample
 
 import de.dailab.jiacpp.container.ContainerAgent
 import de.dailab.jiacpp.model.AgentContainerImage
+import de.dailab.jiacpp.util.ConfigLoader
 import de.dailab.jiacvi.communication.LocalBroker
 import de.dailab.jiacvi.dsl.agentSystem
 
 fun main() {
-
-    // could also be provided as a JSON file in the resources dir
-    val image = AgentContainerImage(
-            "sample-agent-container-image",
-            listOf("sample-requirement"),
-            listOf("sample-provision"),
-            "Sample JIAC++ Agent Container",
-            "Just a sample container doing nothing but receiving and reacting to API calls",
-            "DAI-Lab"
-    )
-
+    val image = ConfigLoader.loadContainerImageFromResources("/container.json")
     agentSystem("jiacpp-sample-container") {
         enable(LocalBroker)
         agents {
