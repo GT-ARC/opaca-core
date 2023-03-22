2ee725038ab72764a31039b9da189fa171d7a6b7
Tobias Küster <tobias.kuester@dai-labor.de>
2023-03-22 17:43:12 +0100

    unit tests for forward restrictions, fixed always-forward in send

--- jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/PlatformImpl.java
+++ jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/PlatformImpl.java
@@ -75,7 +75,7 @@ public class PlatformImpl implements RuntimePlatformApi {
 
     @Override
     public void send(String agentId, Message message, boolean forward) throws NoSuchElementException {
-        var clients = getClients(null, agentId, null, true);
+        var clients = getClients(null, agentId, null, forward);
 
         for (ApiProxy client: (Iterable<? extends ApiProxy>) clients::iterator) {
             log.info("Forwarding /send to " + client.baseUrl);
