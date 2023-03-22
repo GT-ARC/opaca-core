4749230ed41d365af1e6e40137beaee4e34bc0b7
Oskar Felix Kupke <oskarfelix.kupke@gt-arc.com>
2023-03-22 15:48:29 +0100

    added new test for checking if container notify works

--- jiacpp-platform/src/test/java/de/dailab/jiacpp/platform/tests/PlatformTests.java
+++ jiacpp-platform/src/test/java/de/dailab/jiacpp/platform/tests/PlatformTests.java
@@ -1,6 +1,7 @@
 package de.dailab.jiacpp.platform.tests;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import de.dailab.jiacpp.model.*;
 import de.dailab.jiacpp.plattform.Application;
 import de.dailab.jiacpp.util.RestHelper;
@@ -10,8 +11,7 @@ import org.junit.runners.MethodSorters;
 
 import org.springframework.boot.SpringApplication;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
@@ -43,7 +43,8 @@ public class PlatformTests {
     private final String PLATFORM_A = "http://localhost:" + PLATFORM_A_PORT;
     private final String PLATFORM_B = "http://localhost:" + PLATFORM_B_PORT;
 
-    private final String TEST_IMAGE = "registry.gitlab.dai-labor.de/pub/unit-tests/jiacpp-sample-container";
+    //private final String TEST_IMAGE = "registry.gitlab.dai-labor.de/pub/unit-tests/jiacpp-sample-container";
+    private final String TEST_IMAGE = "sample-agent-container-image";
 
     private static String containerId = null;
     private static String platformABaseUrl = null;
@@ -423,8 +424,6 @@ public class PlatformTests {
         Assert.assertEquals(200, con2.getResponseCode());
     }
 
-    // TODO improve notify-test by actually having the sample container update its information (requires some extensions in the sample-container-agent code, though)
-
     @Test
     public void test7RequestInvalidNotify() throws Exception {
         // invalid container
@@ -434,6 +433,25 @@ public class PlatformTests {
         // invalid platform
         var con2 = request(PLATFORM_A, "POST", "/connections/notify", "platform-does-not-exist");
         Assert.assertEquals(404, con2.getResponseCode());
+    }
+
+    @Test
+    public void test7AddNewAction() throws Exception {
+        // create new agent action
+        var con1 = request(PLATFORM_A, "POST", "/invoke/CreateAction/sample1", Map.of("name", "TemporaryTestAction"));
+        Assert.assertEquals(200, con1.getResponseCode());
+
+        // new action has been created, but platform has not yet been notified --> action is unknown
+        con1 = request(PLATFORM_A, "POST", "/invoke/TemporaryTestAction/sample1", Map.of());
+        Assert.assertEquals(404, con1.getResponseCode());
+
+        // notify platform about updates in container, after which the new action is known
+        con1 = request(PLATFORM_A, "POST", "/containers/notify", containerId);
+        Assert.assertEquals(200, con1.getResponseCode());
+
+        // try to invoke the new action, which should now succeed
+        con1 = request(PLATFORM_A, "POST", "/invoke/TemporaryTestAction/sample1", Map.of());
+        Assert.assertEquals(200, con1.getResponseCode());
     }
 
     /*
