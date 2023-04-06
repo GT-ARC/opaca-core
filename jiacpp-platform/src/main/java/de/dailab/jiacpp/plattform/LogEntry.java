36bcb6d0b8725bc50f6f159f0491fea1249da741
acar <benjamin.acar@dai-labor.de>
2023-04-06 15:39:37 +0200

    timestamp, unique IDs for events etc.

--- jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/LogEntry.java
+++ jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/LogEntry.java
@@ -1,12 +1,18 @@
 package de.dailab.jiacpp.plattform;
 
 import lombok.Data;
+import java.sql.Timestamp;
+import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LogEntry {
     private String methodName;
     private Object[] inputParams;
     private Object result;
-    private boolean success;
-    private String errorMessage;
+    private final Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    private final String uniqueId = UUID.randomUUID().toString();
+    private String eventType;
+    private String relatedId;
 }
--- jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/LoggingProxy.java
+++ jiacpp-platform/src/main/java/de/dailab/jiacpp/plattform/LoggingProxy.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.UUID;
 import java.util.List;
 import de.dailab.jiacpp.plattform.LogEntry;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -21,27 +22,41 @@ public class LoggingProxy<T> implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        LogEntry logEntry = new LogEntry();
-        logEntry.setMethodName(method.getName());
-        logEntry.setInputParams(args);
+        LogEntry callEntry = new LogEntry();
+        LogEntry resultEntry = new LogEntry();
+
+        // entry for API call
+        callEntry.setMethodName(method.getName());
+        callEntry.setInputParams(args);
+        callEntry.setEventType("APICall");
+
+        // ID for mapping the related events
+        String relatedId = UUID.randomUUID().toString();
+        resultEntry.setRelatedId(relatedId);
+        callEntry.setRelatedId(relatedId);
+
 
         Object result = null;
         try {
             result = method.invoke(target, args);
-            logEntry.setResult(result);
-            logEntry.setSuccess(true);
+            callEntry.setResult("APICall done.");
+
+            // create a new LogEntry for the result
+            resultEntry.setEventType("APIResult");
+            resultEntry.setResult(result);
+
         } catch (InvocationTargetException e) {
-            logEntry.setSuccess(false);
-            logEntry.setErrorMessage(e.getCause().getMessage());
+            resultEntry.setResult(e.getCause().getMessage());
+            resultEntry.setEventType("APIError");
             throw e.getCause();
         } catch (Exception e) {
-            logEntry.setSuccess(false);
-            logEntry.setErrorMessage(e.getMessage());
+            resultEntry.setResult(e.getMessage());
+            resultEntry.setEventType("APIError");
             throw e;
         } finally {
-            LoggingContext.getInstance().addLogEntry(logEntry);
+            LoggingContext.getInstance().addLogEntry(resultEntry);
+            LoggingContext.getInstance().addLogEntry(callEntry);
         }
-
         // Trigger the function to print the log object as JSON
         printLogEntryJson();
 
