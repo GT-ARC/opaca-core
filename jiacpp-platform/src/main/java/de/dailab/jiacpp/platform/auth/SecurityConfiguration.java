529cf6cc4fa92d6843c7c1ca1190ac3b60067a92
Acar, Benjamin <benjamin.acar@dai-labor.de>
2023-06-05 15:25:48 +0200

    removed one unnecessary import

--- jiacpp-platform/src/main/java/de/dailab/jiacpp/platform/auth/SecurityConfiguration.java
+++ jiacpp-platform/src/main/java/de/dailab/jiacpp/platform/auth/SecurityConfiguration.java
@@ -1,7 +1,5 @@
 package de.dailab.jiacpp.platform.auth;
 
-import javax.annotation.PostConstruct;
-
 import de.dailab.jiacpp.platform.PlatformConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
