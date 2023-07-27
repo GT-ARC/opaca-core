ab8792085d509dee0e5b66b467af3b695af66ceb
Acar, Benjamin <benjamin.acar@dai-labor.de>
2023-07-27 14:22:49 +0200

    data akquisition container

---
+++ examples/data-container/src/main/kotlin/de/dailab/jiacpp/sample/DataAgent.kt
@@ -0,0 +1,95 @@
+package de.dailab.jiacpp.sample
+
+import de.dailab.jiacpp.api.AgentContainerApi
+import de.dailab.jiacpp.container.AbstractContainerizedAgent
+import de.dailab.jiacpp.container.Invoke
+import de.dailab.jiacpp.model.Action
+import de.dailab.jiacpp.model.AgentDescription
+import de.dailab.jiacpp.model.Message
+import de.dailab.jiacvi.behaviour.act
+
+
+class DataAgent(name: String): AbstractContainerizedAgent(name=name) {
+
+    private var lastMessage: Any? = null
+    private var lastBroadcast: Any? = null
+
+    private var extraActions = mutableListOf<Action>()
+
+    override fun getDescription() = AgentDescription(
+        this.name,
+        this.javaClass.name,
+        listOf(
+            Action("AkquireData", mapOf(Pair("camera_id", "String"), Pair("stream_seconds", "Int")), "String"),
+            Action("GetInfo", mapOf(), "Map"),
+            Action("Add", mapOf(Pair("x", "String"), Pair("y", "Int")), "Int"),
+            Action("Fail", mapOf(), "void"),
+            // actions for testing modifying agents and actions at runtime
+            Action("CreateAction", mapOf(Pair("name", "String"), Pair("notify", "Boolean")), "void"),
+            Action("SpawnAgent", mapOf(Pair("name", "String")), "void"),
+            Action("Deregister", mapOf(), "void")
+        ).plus(extraActions)
+    )
+
+    override fun behaviour() = act {
+
+        on<Message> {
+            log.info("ON $it")
+            lastMessage = it.payload
+        }
+
+        listen<Message>("topic") {
+            log.info("LISTEN $it")
+            lastBroadcast = it.payload
+        }
+
+        respond<Invoke, Any?> {
+            log.info("RESPOND $it")
+            when (it.name) {
+                "AkquireData" -> actionAkquireData(it.parameters["camera_id"]!!.asText(), it.parameters["stream_seconds"]!!.asInt())
+                "Add" -> actionAdd(it.parameters["x"]!!.asInt(), it.parameters["y"]!!.asInt())
+                "GetInfo" -> actionGetInfo()
+                "Fail" -> actionFail()
+                "CreateAction" -> createAction(it.parameters["name"]!!.asText(), it.parameters["notify"]!!.asBoolean())
+                "SpawnAgent" -> spawnAgent(it.parameters["name"]!!.asText())
+                "Deregister" -> deregister(false)
+                in extraActions.map { a -> a.name } -> "Called extra action ${it.name}"
+                else -> null
+            }
+        }
+
+    }
+
+    private fun actionAkquireData(camera_id: String, stream_seconds: Int): String {
+        log.info("in 'AkquireData' action, waiting...")
+        println(camera_id)
+        Thread.sleep(1000 * stream_seconds.toLong())
+        log.info("done waiting")
+        return "Action 'AkquireData' of $name called with camera_id=$camera_id and stream_seconds=$stream_seconds"
+    }
+
+    private fun actionAdd(x: Int, y: Int) = x + y
+
+    private fun actionFail() {
+        throw RuntimeException("Action Failed (as expected)")
+    }
+
+    private fun actionGetInfo() = mapOf(
+        Pair("name", name),
+        Pair("lastMessage", lastMessage),
+        Pair("lastBroadcast", lastBroadcast),
+        Pair(AgentContainerApi.ENV_CONTAINER_ID, System.getenv(AgentContainerApi.ENV_CONTAINER_ID)),
+        Pair(AgentContainerApi.ENV_PLATFORM_URL, System.getenv(AgentContainerApi.ENV_PLATFORM_URL)),
+        Pair(AgentContainerApi.ENV_TOKEN, System.getenv(AgentContainerApi.ENV_TOKEN))
+    )
+
+    private fun createAction(name: String, notify: Boolean) {
+        extraActions.add(Action(name, mapOf(), "String"))
+        register(notify)
+    }
+
+    private fun spawnAgent(name: String) {
+        system.spawnAgent(DataAgent(name))
+    }
+
+}
---
+++ examples/data-container/src/main/kotlin/de/dailab/jiacpp/sample/Main.kt
@@ -0,0 +1,17 @@
+package de.dailab.jiacpp.sample
+
+import de.dailab.jiacpp.container.ContainerAgent
+import de.dailab.jiacpp.util.ConfigLoader
+import de.dailab.jiacvi.communication.LocalBroker
+import de.dailab.jiacvi.dsl.agentSystem
+
+fun main() {
+    val image = ConfigLoader.loadContainerImageFromResources("/container.json")
+    agentSystem("jiacpp-sample-container") {
+        enable(LocalBroker)
+        agents {
+            add(ContainerAgent(image))
+            add(DataAgent("Data-Agent"))
+        }
+    }.start()
+}
