a50657c60e08de588eea578574857c4e119f095b
Robert Strehlow <robstrehlow@msn.com>
2023-11-09 10:03:42 +0100

    Support Role creation without privileges

--- jiacpp-platform/src/main/java/de/dailab/jiacpp/platform/auth/Role.java
+++ jiacpp-platform/src/main/java/de/dailab/jiacpp/platform/auth/Role.java
@@ -10,6 +10,10 @@ public class Role {
     private String name;
     private Collection<String> privileges;
 
+    public Role(String name) {
+        this(name, null);
+    }
+
     public Role(String name, Collection<String> privileges) {
         this.name = name;
         this.privileges = privileges;
