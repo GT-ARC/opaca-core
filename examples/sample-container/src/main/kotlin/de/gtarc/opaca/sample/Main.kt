0183ec0abf4edb6502555b7c970a405e734116a9
Tobias Küster <tobias.kuester@dai-labor.de>
2023-12-16 21:11:37 +0100

    renamed other packages

---
+++ examples/sample-container/src/main/kotlin/de/gtarc/opaca/sample/Main.kt
@@ -0,0 +1,20 @@
+package de.gtarc.opaca.sample
+
+import de.gtarc.opaca.container.ContainerAgent
+import de.gtarc.opaca.util.ConfigLoader
+import de.dailab.jiacvi.communication.LocalBroker
+import de.dailab.jiacvi.dsl.agentSystem
+
+fun main() {
+    val image = ConfigLoader.loadContainerImageFromResources("/container.json")
+    agentSystem("jiacpp-sample-container") {
+        enable(LocalBroker)
+        agents {
+            add(ContainerAgent(image))
+            add(SampleAgent("sample1"))
+            add(SampleAgent("sample2"))
+            add(SimpleUIAgent())
+            add(SimpleUDPAgent())
+        }
+    }.start()
+}
