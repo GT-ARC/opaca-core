e7367a37ee69b967456c404e6f6741156d469d07
Strehlow, Robert <Robert.Strehlow@gt-arc.com>
2024-06-10 14:23:37 +0200

    Fix errors

--- opaca-model/src/main/java/de/gtarc/opaca/util/ActionToOpenApi.java
+++ opaca-model/src/main/java/de/gtarc/opaca/util/ActionToOpenApi.java
@@ -19,71 +19,64 @@ public class ActionToOpenApi {
 
         for (AgentDescription agent : agents) {
             for (Action action : agent.getActions()) {
+
+                // Create a map of names and type for the request body parameters
+                var params = new HashMap<String, Object>();
+                for (String name : action.getParameters().keySet()) {
+                    params.put(name, Map.of("type", action.getParameters().get(name).getType()));
+                }
+
+                // The request body map holding the action parameters
                 var requestBody = Map.of(
                         "content", Map.of(
                                 "application/json", Map.of(
                                         "schema", Map.of(
                                                 "type", "object",
-                                                "properties", action.getParameters() // TODO check if this is OpenAPI conform
+                                                "properties", params // TODO check if this is OpenAPI conform
                                         )
                                 )
                         ),
                         "required", true
                 );
 
+                // Responses that could be retrieved by calling an action
                 var responses = Map.of(
                         "200", Map.of(
                                 "description", "OK",
                                 "content", Map.of(
                                         "*/*", Map.of(
-                                                "schema", action.getResult()
+                                                "schema", Map.of(
+                                                        "type", action.getResult().getType(),
+                                                        "items", action.getResult().getItems() == null ? Map.of() : action.getResult().getItems()
+                                                )
                                         )
                                 )
                         ),
                         "default", Map.of(
                                 "description", "Unexpected error",
                                 "content", Map.of(
-                                        "*/*", Map.of(
-                                                "schema", "#/components/schemas/ErrorModel"
+                                        "application/json", Map.of(
+                                                "schema", Map.of("$ref", "#/components/schemas/Error")
                                         )
                                 )
                         )
                         // TODO Further error responses necessary?
                 );
 
+                // The path combines request body and responses
+                // Includes the action description (if available), action name and query parameters
                 var path = Map.of(
                         "post", Map.of(
                                 "tags", List.of("agent"),
                                 "description", action.getDescription() == null ? "" : action.getDescription(),
                                 "operationId", action.getName(),
                                 "parameters", List.of(
-                                        Map.of(
-                                                "name", "timeout",
-                                                "in", "query",
-                                                "description", "Timeout in seconds after which the action should abort",
-                                                "required", false,
-                                                "allowEmptyValue", true
-                                        ),
-                                        Map.of(
-                                                "name", "containerId",
-                                                "in", "query",
-                                                "description", "Id of the container where the agent is running",
-                                                "required", false,
-                                                "allowEmptyValue", true
-                                        ),
-                                        Map.of(
-                                                "name", "forward",
-                                                "in", "query",
-                                                "description", "Whether or not to include the connected runtime platforms",
-                                                "required", false,
-                                                "allowEmptyValue", true
-                                        )
+                                        Map.of("$ref", "#/components/parameters/timeoutParam"),
+                                        Map.of("$ref", "#/components/parameters/containerIdParam"),
+                                        Map.of("$ref", "#/components/parameters/forwardParam")
                                 ),
                                 "requestBody", requestBody,
-                                "responses", responses,
-                                "security", List.of(
-                                        Map.of("bearerAuth", List.of())
-                                )
+                                "responses", responses
                         )
                 );
 
@@ -92,13 +85,69 @@ public class ActionToOpenApi {
         }
 
         var root = Map.of(
-            "openapi", "3.1.0",
-                "info", Map.of(), // TODO Maybe a combination of agent names and agent descriptions if available?
+            "openapi", "3.0.1",
+                "info", Map.of(
+                        "title", "Collection of actions provided by the agents running on the OPACA platform",
+                        "version", "0.2"    // Same as in the opaca-api
+                ),
                 "paths", paths,
-                "components", Map.of(), // TODO what are some schemas and how to use them here?
-                "security", List.of(
-                    Map.of("bearerAuth", List.of())
+                "components", Map.of(
+                    "schemas", Map.of(
+                        "Error", Map.of(                // General error schema
+                            "type", "object",
+                            "properties", Map.of(
+                                "code", Map.of(
+                                    "type", "integer"
+                                ),
+                                "message", Map.of(
+                                    "type", "string"
+                                )
+                            )
+                        )
+                    ),
+                    "parameters", Map.of(               // Definition of query params
+                        "timeoutParam", Map.of(
+                            "name", "timeout",
+                            "in", "query",
+                            "description", "Timeout in seconds after which the action should abort",
+                            "required", false,
+                            "allowEmptyValue", true,
+                            "schema", Map.of(
+                                "type", "integer"
+                            )
+                        ),
+                        "containerIdParam", Map.of(
+                            "name", "containerId",
+                            "in", "query",
+                            "description", "Id of the container where the agent is running",
+                            "required", false,
+                            "allowEmptyValue", true,
+                            "schema", Map.of(
+                                "type", "string"
+                            )
+                        ),
+                        "forwardParam", Map.of(
+                            "name", "forward",
+                            "in", "query",
+                            "description", "Whether or not to include the connected runtime platforms",
+                            "required", false,
+                            "allowEmptyValue", true,
+                            "schema", Map.of(
+                                "type", "boolean"
+                            )
+                        )
+                    ),
+                "securitySchemes", Map.of(              // Definition for bearer authorization with JWT
+                "bearerAuth", Map.of(
+                    "type", "http",
+                    "scheme", "bearer",
+                    "bearerFormat", "JWT"
+                    )
                 )
+            ),
+            "security", List.of(
+                Map.of("bearerAuth", List.of())
+            )
         );
 
         return RestHelper.mapper.convertValue(root, JsonNode.class);
