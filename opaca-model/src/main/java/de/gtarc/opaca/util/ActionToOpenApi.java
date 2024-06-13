d0c6cb2e74429477e680a74a76c6a5fd6cd2a5d9
Strehlow, Robert <robert.strehlow@gt-arc.com>
2024-06-13 11:34:16 +0200

    Include reference to custom data types

--- opaca-model/src/main/java/de/gtarc/opaca/util/ActionToOpenApi.java
+++ opaca-model/src/main/java/de/gtarc/opaca/util/ActionToOpenApi.java
@@ -1,18 +1,29 @@
 package de.gtarc.opaca.util;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
 import de.gtarc.opaca.model.Action;
 import de.gtarc.opaca.model.AgentDescription;
+import de.gtarc.opaca.model.Parameter;
 
 /**
  * ActionToOpenApi
  */
 public class ActionToOpenApi {
+
+    private static final Set<String> STANDARD_TYPES = new HashSet<>();
+
+    // A list of standard openapi data types
+    static {
+        STANDARD_TYPES.add("string");
+        STANDARD_TYPES.add("number");
+        STANDARD_TYPES.add("integer");
+        STANDARD_TYPES.add("boolean");
+        STANDARD_TYPES.add("array");
+        STANDARD_TYPES.add("object");
+    }
 
     public static JsonNode createOpenApiSchema(List<AgentDescription> agents) {
         var paths = new HashMap<String, Object>();
@@ -21,9 +32,19 @@ public class ActionToOpenApi {
             for (Action action : agent.getActions()) {
 
                 // Create a map of names and type for the request body parameters
+                // If type is array, add additional items set
                 var params = new HashMap<String, Object>();
                 for (String name : action.getParameters().keySet()) {
-                    params.put(name, Map.of("type", action.getParameters().get(name).getType()));
+                    var type = checkCustomType(action.getParameters().get(name).getType());
+                    if (type == "array") {
+                        params.put(name, Map.of("type", type, "items", processArrayItems(action.getParameters().get(name).getItems())));
+                    }
+                    else if (type instanceof Map) {
+                        params.put(name, type);
+                    }
+                    else {
+                        params.put(name, Map.of("type", type));
+                    }
                 }
 
                 // The request body map holding the action parameters
@@ -39,16 +60,33 @@ public class ActionToOpenApi {
                         "required", true
                 );
 
+                // Check if action expects a result
+                // If no result is expected, default response type is integer but nullable is set to true
+                var schema = new HashMap<>();
+                if (action.getResult() == null) {
+                    schema.put("type", "integer");
+                    schema.put("nullable", true);
+                }
+                // Otherwise, use type information from json schema of action
+                else {
+                    var type = checkCustomType(action.getResult().getType());
+                    if (type instanceof Map) {
+                        schema.putAll((Map<?, ?>) type);
+                    }
+                    else {
+                        schema.put("type", type);
+                    }
+                    schema.put("nullable", false);
+                    schema.put("items", action.getResult().getItems() == null ? Map.of() : processArrayItems(action.getResult().getItems()));
+                }
+
                 // Responses that could be retrieved by calling an action
                 var responses = Map.of(
                         "200", Map.of(
                                 "description", "OK",
                                 "content", Map.of(
                                         "*/*", Map.of(
-                                                "schema", Map.of(
-                                                        "type", action.getResult().getType(),
-                                                        "items", action.getResult().getItems() == null ? Map.of() : action.getResult().getItems()
-                                                )
+                                                "schema", schema
                                         )
                                 )
                         ),
@@ -69,7 +107,7 @@ public class ActionToOpenApi {
                         "post", Map.of(
                                 "tags", List.of("agent"),
                                 "description", action.getDescription() == null ? "" : action.getDescription(),
-                                "operationId", action.getName(),
+                                "operationId", agent.getAgentType() + "-" + action.getName(),   // Should be a unique string
                                 "parameters", List.of(
                                         Map.of("$ref", "#/components/parameters/timeoutParam"),
                                         Map.of("$ref", "#/components/parameters/containerIdParam"),
@@ -86,57 +124,57 @@ public class ActionToOpenApi {
 
         var root = Map.of(
             "openapi", "3.0.1",
-                "info", Map.of(
-                        "title", "Collection of actions provided by the agents running on the OPACA platform",
-                        "version", "0.2"    // Same as in the opaca-api
-                ),
-                "paths", paths,
-                "components", Map.of(
-                    "schemas", Map.of(
-                        "Error", Map.of(                // General error schema
-                            "type", "object",
-                            "properties", Map.of(
-                                "code", Map.of(
-                                    "type", "integer"
-                                ),
-                                "message", Map.of(
-                                    "type", "string"
-                                )
-                            )
-                        )
-                    ),
-                    "parameters", Map.of(               // Definition of query params
-                        "timeoutParam", Map.of(
-                            "name", "timeout",
-                            "in", "query",
-                            "description", "Timeout in seconds after which the action should abort",
-                            "required", false,
-                            "allowEmptyValue", true,
-                            "schema", Map.of(
+            "info", Map.of(
+                "title", "Collection of actions provided by the agents running on the OPACA platform",
+                "version", "0.2"    // Same as in the opaca-api
+            ),
+            "paths", paths,
+            "components", Map.of(
+                "schemas", Map.of(
+                    "Error", Map.of(                // General error schema
+                        "type", "object",
+                        "properties", Map.of(
+                            "code", Map.of(
                                 "type", "integer"
-                            )
-                        ),
-                        "containerIdParam", Map.of(
-                            "name", "containerId",
-                            "in", "query",
-                            "description", "Id of the container where the agent is running",
-                            "required", false,
-                            "allowEmptyValue", true,
-                            "schema", Map.of(
+                            ),
+                            "message", Map.of(
                                 "type", "string"
                             )
-                        ),
-                        "forwardParam", Map.of(
-                            "name", "forward",
-                            "in", "query",
-                            "description", "Whether or not to include the connected runtime platforms",
-                            "required", false,
-                            "allowEmptyValue", true,
-                            "schema", Map.of(
-                                "type", "boolean"
-                            )
+                        )
+                    )
+                ),
+                "parameters", Map.of(               // Definition of query params
+                    "timeoutParam", Map.of(
+                        "name", "timeout",
+                        "in", "query",
+                        "description", "Timeout in seconds after which the action should abort",
+                        "required", false,
+                        "allowEmptyValue", true,
+                        "schema", Map.of(
+                            "type", "integer"
                         )
                     ),
+                    "containerIdParam", Map.of(
+                        "name", "containerId",
+                        "in", "query",
+                        "description", "Id of the container where the agent is running",
+                        "required", false,
+                        "allowEmptyValue", true,
+                        "schema", Map.of(
+                            "type", "string"
+                        )
+                    ),
+                    "forwardParam", Map.of(
+                        "name", "forward",
+                        "in", "query",
+                        "description", "Whether or not to include the connected runtime platforms",
+                        "required", false,
+                        "allowEmptyValue", true,
+                        "schema", Map.of(
+                            "type", "boolean"
+                        )
+                    )
+                ),
                 "securitySchemes", Map.of(              // Definition for bearer authorization with JWT
                 "bearerAuth", Map.of(
                     "type", "http",
@@ -151,6 +189,37 @@ public class ActionToOpenApi {
         );
 
         return RestHelper.mapper.convertValue(root, JsonNode.class);
+    }
+
+    // If the type is part of the default data types, return type as string
+    // Otherwise a reference to the custom type will be returned
+    private static Object checkCustomType(String type) {
+        return STANDARD_TYPES.contains(type) ? type : Map.of("$ref", "#/components/schemas/" + type);
+    }
+
+    // Recursively checks the contents of the ArrayItems type of the Parameter
+    // If a default data type is used, returns ("type", <type>)
+    // If a custom type is used, return a reference ("$ref", <reference-url>)
+    private static Map<?, ?> processArrayItems(Parameter.ArrayItems arrayItems) {
+        Object typeSchema = checkCustomType(arrayItems.getType());
+
+        var result = new HashMap<>();
+
+        // If type is array, recursively add items to array type
+        if (arrayItems.getItems() != null) {
+            result.put("type", typeSchema);
+            result.put("items", processArrayItems(arrayItems.getItems()));
+        }
+        // If type is a map, it is a reference to a custom type, put into field "$ref"
+        else if (typeSchema instanceof Map) {
+            result.putAll((Map<?, ?>) typeSchema);
+        }
+        // If type is a default data type, put type description into field "type"
+        else {
+            result.put("type", typeSchema);
+        }
+
+        return result;
     }
 
 }
