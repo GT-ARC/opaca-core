03897708f5d492bc8ac8c2604518f82588b129f0
Tobias Küster <tobias.kuester@dai-labor.de>
2025-06-13 14:15:21 +0200

    new port check logic that works when RP in docker, by checking ports on host machine

--- opaca-platform/src/main/java/de/gtarc/opaca/platform/containerclient/AbstractContainerClient.java
+++ opaca-platform/src/main/java/de/gtarc/opaca/platform/containerclient/AbstractContainerClient.java
@@ -1,14 +1,10 @@
 package de.gtarc.opaca.platform.containerclient;
 
-import com.google.common.base.Strings;
-import de.gtarc.opaca.model.AgentContainer;
-import de.gtarc.opaca.model.AgentContainerImage;
+import lombok.extern.java.Log;
 
 import java.io.IOException;
-import java.net.DatagramSocket;
-import java.net.ServerSocket;
+import java.net.*;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Common superclass for different Container Clients implementing some common functions
@@ -30,7 +26,24 @@ abstract public class AbstractContainerClient implements ContainerClient {
 
     private boolean isPortAvailable(int port, Set<Integer> newPorts) {
         if (usedPorts.contains(port) || newPorts.contains(port)) return false;
-        try (var s1 = new ServerSocket(port); var s2 = new DatagramSocket(port)) {
+        var host = getContainerBaseUrl().replaceAll("^\\w+://", "");
+        return ! isTcpPortOpen(host, port) && ! isUdpPortOpen(host, port);
+    }
+
+    private static boolean isTcpPortOpen(String host, int port) {
+        try (Socket socket = new Socket(host, port)) {
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    private static boolean isUdpPortOpen(String host, int port) {
+        try (DatagramSocket socket = new DatagramSocket()) {
+            socket.setSoTimeout(1000);
+            byte[] buffer = new byte[1];
+            socket.send(new DatagramPacket(buffer, buffer.length, InetAddress.getByName(host), port));
+            socket.receive(new DatagramPacket(buffer, buffer.length)); // assuming response by server
             return true;
         } catch (IOException e) {
             return false;
