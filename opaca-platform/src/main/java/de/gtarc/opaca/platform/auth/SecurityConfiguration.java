c05bbb32765cc85fc05de8ad1b7296e1bc047083
Tobias Küster <tobias.kuester@dai-labor.de>
2026-01-26 15:49:27 +0100

    moved role-hierarchy back into code -> easier KC setup

--- opaca-platform/src/main/java/de/gtarc/opaca/platform/auth/SecurityConfiguration.java
+++ opaca-platform/src/main/java/de/gtarc/opaca/platform/auth/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -77,6 +79,17 @@ public class SecurityConfiguration {
         // authorization rules with required-auth and without
         http.authorizeHttpRequests(authPolicy);
         return http.build();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("""
+                ROLE_MANAGER > ROLE_CONTRIBUTOR
+                ROLE_CONTRIBUTOR > ROLE_USER
+                ROLE_USER > ROLE_GUEST
+                """);
+        return roleHierarchy;
     }
 
 }
