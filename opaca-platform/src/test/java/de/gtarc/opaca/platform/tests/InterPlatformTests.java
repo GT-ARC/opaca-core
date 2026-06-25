8c2f90d24252539af54bc3be50e9ba1d41c15377
Tobias Küster <tobias.kuester@dai-labor.de>
2026-06-25 16:53:01 +0200

    forgot to include containers of self...

--- opaca-platform/src/test/java/de/gtarc/opaca/platform/tests/InterPlatformTests.java
+++ opaca-platform/src/test/java/de/gtarc/opaca/platform/tests/InterPlatformTests.java
@@ -85,6 +85,9 @@ public class InterPlatformTests {
         con = request(PLATFORM_B_URL, "GET", "/agents", null);
         res = result(con, List.class);
         Assert.assertEquals(0, res.size());
+        con = request(PLATFORM_A_URL, "GET", "/agents?includeConnected=true", null);
+        res = result(con, List.class);
+        Assert.assertEquals(2, res.size());
         con = request(PLATFORM_B_URL, "GET", "/agents?includeConnected=true", null);
         res = result(con, List.class);
         Assert.assertEquals(2, res.size());
@@ -101,6 +104,9 @@ public class InterPlatformTests {
         con = request(PLATFORM_B_URL, "GET", "/containers", null);
         res = result(con, List.class);
         Assert.assertEquals(0, res.size());
+        con = request(PLATFORM_A_URL, "GET", "/containers?includeConnected=true", null);
+        res = result(con, List.class);
+        Assert.assertEquals(1, res.size());
         con = request(PLATFORM_B_URL, "GET", "/containers?includeConnected=true", null);
         res = result(con, List.class);
         Assert.assertEquals(1, res.size());
